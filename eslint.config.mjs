2354907f42547243193fd184ad43fb532702f771
erdilalbayrak <ealbayrak@gmail.com>
2025-07-03 00:56:40 +0200

    [AR-1765] Improve eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add a custom rules block to override TypeScript rules
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // 👈 disables the rule
+    },
+  },
 ];
 
 export default eslintConfig;
